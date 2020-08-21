dbe6c7f664c065b1cea0758ac2cd6ba6f7511b9c
Sule Sinikoglu <ssinikoglu@gmail.com>
2020-08-21 11:39:14 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your code here
+//Breakfast class
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
+//New instance and arguments of breakfast class
+let bfast = new Breakfast("eggs", "juice");
+
+//Lunch class
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+//New instance and arguments of lunch class
+let newlunch = new Lunch("side salad", "broccoli cheddar soup", "iced tea");
+
+//Dinner class
+class Dinner {
+  constructor(salad, soup, entree, dessert) {
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this._dessert = dessert;
+  }
+}
+//New instance and arguments of dinner class includes a private property
+let newdinner = new Dinner('balsamic salad', 'consomme', 'filet mignon', 'cheesecake');
+
+
+
+
